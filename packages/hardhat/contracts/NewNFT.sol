f031a13fb288eee000442868752a6586abcd1429
Shiv Bhonde <shivbhonde04@gmail.com>
2022-12-03 11:43:09 +0530

    optimize NewNFT contract

--- packages/hardhat/contracts/NewNFT.sol
+++ packages/hardhat/contracts/NewNFT.sol
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {IConnext} from "@connext/nxtp-contracts/contracts/core/connext/interfaces/IConnext.sol";
 import {Base64} from "./Base64.sol";
 
-contract NFC is ERC721URIStorage, Ownable, ReentrancyGuard {
+contract NFC is ERC721Enumerable, ReentrancyGuard {
     uint32 public constant DOMAIN_ID = 9991; //swap domain id
 
     string[3] public colors = ["#CD7F32", "#E5E4E2", "#FFD700"];
+    mapping(uint256 => string) idToColor;
 
     IConnext public immutable connext;
 
@@ -32,13 +31,23 @@ contract NFC is ERC721URIStorage, Ownable, ReentrancyGuard {
         bytes memory _callData
     ) external returns (bytes memory) {
         // Unpack the _callData
-        (address _contributor, uint256 _contributedAmount) = abi.decode(_callData, (address,uint256));
+        (address _contributor, uint256 _contributedAmount) = abi.decode(
+            _callData,
+            (address, uint256)
+        );
         _tokenIds.increment();
+
         {
-        uint256 _id = _tokenIds.current();
-        _mint(_contributor, _id);
-        // we will pass amount to `generateSVG`
-        _setTokenURI(_id, generateSVG(_contributedAmount));
+            uint256 _id = _tokenIds.current();
+            if (_contributedAmount <= 0.05 ether) {
+                idToColor[_id] = "#CD7F32";
+            } else if (_contributedAmount <= 0.1 ether) {
+                idToColor[_id] = "#E5E4E2";
+            } else {
+                idToColor[_id] = "#FFD700";
+            }
+
+            _mint(_contributor, _id);
         }
     }
 
@@ -65,65 +74,49 @@ contract NFC is ERC721URIStorage, Ownable, ReentrancyGuard {
         return _tokenIds.current();
     }
 
-    function generateSVG(uint256 _amount)
-        private
-        pure
-        returns (string memory)
-    {
-        string memory strId = Strings.toString(_amount);
-        string[12] memory value;
+    function tokenURI(
+        uint256 id
+    ) public view override returns (string memory json) {
+        require(_exists(id), "!exist");
 
-        value[
-            0
-        ] = '<svg width="250" height="150" style="border:1px solid red; background-color: gold">';
-        value[1] = '<text x="20" y="25" fill="purple"> Title :';
-        value[2] = "This is proposal";
-        value[3] = "</text>";
-        value[4] = '<text x="20" y="55" fill="purple"> Benefits : </text>';
-        value[6] = '<text x="20" y="75" fill="purple">1)';
-        value[7] = "60% off on medicines";
-        value[8] = "</text>";
-        value[9] = '<text x="20" y="95" fill="purple">2)';
-        value[10] = "Life time free home delivery";
-        value[11] = "</text>";
-        
-        // string memory finalSvg = string(
-        //     abi.encodePacked(
-        //         value[0],
-        //         value[1],
-        //         value[2],
-        //         value[3],
-        //         value[4],
-        //         value[5],
-        //         value[6],
-        //         value[7],
-        //         value[8],
-        //         value[9],
-        //         value[10],
-        //         value[11],
-        //         "</svg>"
-        //     )
-        // );
+        return generateSVG(id);
+    }
 
-        // string memory json = Base64.encode(
-        //     bytes(
-        //         string(
-        //             abi.encodePacked(
-        //                 '{ "name": "NFC #',
-        //                 strId,
-        //                 '", "description": "NFC is a initicative for wellness and goodness towords the new world.", "image": "data:image/svg+xml;base64,',
-        //                 Base64.encode(bytes(finalSvg)),
-        //                 '"}'
-        //             )
-        //         )
-        //     )
-        // );
+    function generateSVG(uint256 _id) private view returns (string memory) {
+        string memory strId = Strings.toString(_id);
+        string memory certificateBG = idToColor[_id];
 
-        // // string memory finalTokenUri = 
+        string memory finalSvg = string(
+            abi.encodePacked(
+                '<svg width="250" height="150" style="border:1px solid red; background-color: gold">',
+                '<text x="20" y="25" fill="purple"> Title :',
+                "This is proposal",
+                "</text>",
+                '<text x="20" y="55" fill="purple"> Benefits : </text>',
+                '<text x="20" y="75" fill="purple">1)',
+                "60% off on medicines",
+                "</text>",
+                '<text x="20" y="95" fill="purple">2)',
+                "Life time free home delivery",
+                "</text>",
+                "</svg>"
+            )
+        );
 
-        // return string(
-        //     abi.encodePacked("data:application/json;base64,", json)
-        // );
-        
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{ "name": "NFC #',
+                        strId,
+                        '", "description": "NFC is a initicative for wellness and goodness towords the new world.", "image": "data:image/svg+xml;base64,',
+                        Base64.encode(bytes(finalSvg)),
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        return string(abi.encodePacked("data:application/json;base64,", json));
     }
 }
